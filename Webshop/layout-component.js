69d727ce2f60f9745bed5be5111e5759df7fa7d4
Jesper Højlund <github@hillgrove.dk>
2025-02-07 12:19:03 +0100

    feat: add Bootstrap styling and responsive navbar

--- Webshop/layout-component.js
+++ Webshop/layout-component.js
@@ -1,14 +1,34 @@
 export const LayoutComponent = {
     template: `
-        <div>
-            <nav>
-                <ul>
-                    <li><router-link to="/">Home</router-link></li>
-                    <li><router-link to="/register">Register</router-link></li>
-                    <li><router-link to="/login">Login</router-link></li>
-                    <li><router-link to="/about">About</router-link></li>
-                </ul>
+        <div class="container">
+            <nav class="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
+                <div class="container-fluid">
+
+                    <a class="navbar-brand" href="#">Webshop</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li class="nav-item">
+                                <router-link class="nav-link" to="/">Home</router-link>
+                            </li>
+                            <li class="nav-item">
+                                <router-link class="nav-link" to="/register">Register</router-link>
+                            </li>
+                            <li class="nav-item">
+                                <router-link class="nav-link" to="/login">Login</router-link>
+                            </li>
+                            <li class="nav-item">
+                                <router-link class="nav-link" to="/about">About</router-link>
+                            </li>
+                        </ul>
+                    </div>
+
+                </div>
             </nav>
+
             <router-view></router-view>
         </div>
     `
