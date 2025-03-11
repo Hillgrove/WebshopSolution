dda8d191b3eb068530c31b5c6dfb0cdc8a5b53f0
Jesper Højlund <github@hillgrove.dk>
2025-03-11 13:57:56 +0100

    feat: add ProductsController to handle product-related API requests

---
+++ Webshop.API/Controllers/ProductsController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Webshop.Data;
+using Webshop.Data.Models;
+
+
+namespace Webshop.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductsController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        // GET: api/<UsersController>
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<IEnumerable<Product>>> Get()
+        {
+            IEnumerable<Product> products = await _productRepository.GetAllAsync();
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<Product?>> Get(int id)
+        {
+            Product? foundProduct = await _productRepository.GetByIdAsync(id);
+            if (foundProduct is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(foundProduct);
+        }
+    }
+}
