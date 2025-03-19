91b09b4b8edad4a378039300c6eb236e857a1496
Jesper Højlund <github@hillgrove.dk>
2025-03-19 12:06:56 +0100

    fix: Allow ProductId to be nullable and rename PriceInOere to PriceAtPurchaseInOere in OrderItem model

--- Webshop.Data/Models/OrderItem.cs
+++ Webshop.Data/Models/OrderItem.cs
@@ -4,9 +4,9 @@
     {
         public int Id { get; set; }
         public int OrderId { get; set; }
-        public required int ProductId { get; set; }
-        public string? ProductName { get; set; }
+        public int? ProductId { get; set; }
+        public required string ProductName { get; set; }
         public required int Quantity { get; set; }
-        public required int PriceInOere { get; set; }
+        public required int PriceAtPurchaseInOere { get; set; }
     }
 }
